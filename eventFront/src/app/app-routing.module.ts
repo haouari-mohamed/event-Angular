b3a48dac6773cc4ffaf1e215cba9251fa09feb2c
mohamedhri <125465245+mohamedhri@users.noreply.github.com>
2024-08-01 22:32:07 +0100

    update

--- eventFront/src/app/app-routing.module.ts
+++ eventFront/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {RegisterComponent} from "./components/register/register.component";
 import {LoginComponent} from "./components/login/login.component";
-import {DashboardComponent} from "./components/dashboard/dashboard.component";
+import {DashboardComponent} from "./components/User/dashboard/dashboard.component";
 import {AdminComponent} from "./components/admin/admin.component";
+import { EventListComponent } from './components/User/dashboard/events/event-list/event-list.component';
+import { ReservationListComponent } from './components/User/dashboard/reservations/reservation-list/reservation-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/register', pathMatch: 'full' },
@@ -11,6 +13,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'user/dashboard', component: DashboardComponent },
   { path: 'admin/dashboard', component: AdminComponent },
+  {path :'events', component: EventListComponent},
+  {path: 'book', component: ReservationListComponent}
 ];
 
 @NgModule({
